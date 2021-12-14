0d93bc654bcb0e54b500a04607b1b938832933ef
studyingheesu <heesu.naver@gmail.com>
2021-12-14 19:18:16 +0900

    filter by category

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,12 +1,34 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
+type CATEGORY = 'TO_DO' | 'DOING' | 'DONE';
 export interface ITodo {
   text: string;
   id: number;
-  category: 'TO_DO' | 'DOING' | 'DONE';
+  category: CATEGORY;
 }
 
 export const todosState = atom<ITodo[]>({
   key: 'todo',
   default: [],
+});
+
+export type FILTER = CATEGORY | 'ALL';
+
+export const todoFilterState = atom<FILTER>({
+  key: 'todoFilter',
+  default: 'ALL',
+});
+
+export const filteredTodoState = selector({
+  key: 'todoFiltered',
+  get: ({ get }) => {
+    const todos = get(todosState);
+    const filter = get(todoFilterState);
+
+    if (filter === 'ALL') {
+      return todos;
+    } else {
+      return todos.filter((todo) => todo.category === filter);
+    }
+  },
 });
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,17 +1,27 @@
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import styled from 'styled-components';
-import { todosState } from '../atoms';
+import { FILTER, filteredTodoState, todoFilterState } from '../atoms';
 import CreateTodo from './CreateTodo';
 import Todo from './Todo';
 
 const List = styled.ul``;
 
 const TodoList = () => {
-  const todos = useRecoilValue(todosState);
+  const todos = useRecoilValue(filteredTodoState);
+  const [filter, setFilter] = useRecoilState(todoFilterState);
+  const handleChange = ({ target: { value } }: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilter(value as FILTER);
+  };
 
   return (
     <div>
       <CreateTodo />
+      <select value={filter} onChange={handleChange}>
+        <option value="ALL">All</option>
+        <option value="TO_DO">To Do</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">Done</option>
+      </select>
       <List>
         {todos.map((todo) => (
           <Todo key={todo.id} {...todo} />
